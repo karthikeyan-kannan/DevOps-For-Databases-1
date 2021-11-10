1e607922672c528638fdc7d5ccb5f5249e401945
Karthikeyan Kannan <Karthikeyan.Kannan@live.com>
2021-11-10 21:08:09 +0530

    windows

--- vmcreate/Windows/main.tf
+++ vmcreate/Windows/main.tf
@@ -15,12 +15,39 @@ resource "azurerm_virtual_network" "vnet"{
 
 }
 
+resource "azurerm_network_security_group" "nsg" {
+    name                = "${var.resource_prefix}-nsg"
+    location            = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "Allow_RDP"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+}
 resource "azurerm_subnet" "subnet" {
     name                    = "${var.resource_prefix}-subnet"
     resource_group_name     = azurerm_resource_group.rg.name
     virtual_network_name    = azurerm_virtual_network.vnet.name
     address_prefixes        = ["10.0.0.0/24"] 
 
+}
+resource "azurerm_public_ip" "publicip" {
+  name                = "${var.resource_prefix}-pip"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Dynamic"
+  sku                 = "Basic"
+  domain_name_label   = "testvmforkarthik"
+ 
 }
 
 resource "azurerm_network_interface" "nic" {
@@ -32,11 +59,14 @@ resource "azurerm_network_interface" "nic" {
         name                            = "testconfig1"
         subnet_id                       = azurerm_subnet.subnet.id
         private_ip_address_allocation   = "Dynamic"
-
+        public_ip_address_id            = azurerm_public_ip.publicip.id
     }
 
 }
-
+resource "azurerm_network_interface_security_group_association" "nsgassociation" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
 resource "azurerm_windows_virtual_machine" "vm" {
     name                    = "${var.resource_prefix}"
     resource_group_name     = azurerm_resource_group.rg.name
@@ -45,6 +75,7 @@ resource "azurerm_windows_virtual_machine" "vm" {
     size                    = var.vm_size
     admin_username          = var.username
     admin_password          = var.password
+    license_type            = "Windows_Server"
 
     source_image_reference {
         publisher = "MicrosoftWindowsServer"
